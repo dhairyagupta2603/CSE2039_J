be82317ebd7273f232469b2c0f438d1afeca7078
dhairyagupta2603 <dhairyagupta2603@gmail.com>
2022-01-24 22:49:19 +0530

    created piece class and derived player and enemy classes with basic functions

--- objects/constants.py
+++ objects/constants.py
@@ -7,6 +7,12 @@ SQUARE_SIZE = WIDTH // COLS
 GREY = (127, 127, 127)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
 GREEN = (0, 255, 0)
 DARKER_GREEN = (0, 225, 0)
 BLUE = (0, 0, 255)
+
+# piece type
+PLAYER = 'player'
+ENEMY_4_DIR = 'weak_enemy'
+ENEMY_8_DIR = 'strong_enemy'
