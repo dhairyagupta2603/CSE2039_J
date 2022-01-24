2fb7bb643cf64e8d2f7c1041eece496ef5eae2fc
dhairyagupta2603 <dhairyagupta2603@gmail.com>
2022-01-24 21:24:21 +0530

    created basic board structure

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import pygame
 from objects.constants import HEIGHT, WIDTH
-
+from objects.board import Board
 
 FPS = 30
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Kingdom of Circles')
 
+board = Board()
+
 def main():
     run = True
     Clock = pygame.time.Clock()
-
+    
     while run:
         Clock.tick(FPS)
 
@@ -17,7 +19,11 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
+        board.draw_squares(WIN)
+        pygame.display.update()
+        
     pygame.quit()
+
 
 if __name__ == '__main__':
     main()
